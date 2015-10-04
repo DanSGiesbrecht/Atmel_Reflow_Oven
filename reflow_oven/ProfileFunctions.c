590d449c726e751f703da34a48d5582d3234e74a
Daniel Giesbrecht <daniel.soley.giesbrecht@gmail.com>
2015-10-03 22:49:27 -0600

    Added some functions to load EEPROM data into global profile.

--- reflow_oven/ProfileFunctions.c
+++ reflow_oven/ProfileFunctions.c
@@ -7,4 +7,49 @@
 
 #include "ProfileFunctions.h"
 
+/* GLOBAL profile struct, used as the working copy */
+Profile working_profile;
 
+/* DEFINITIONS */
+
+void update_Profile(Profile *_prof, uint8_t **eeprom_ptr)
+{
+    uint8_t SRAM_name_array[NAMESIZE];
+    uint8_t SRAM_time_array[NUM_TIME_PARAMS];
+    uint8_t SRAM_temp_array[NUM_TEMP_PARAMS];
+    
+    eeprom_read_block((void*)SRAM_name_array, (const void*)(eeprom_ptr[0]), NAMESIZE);
+    eeprom_read_block((void*)SRAM_time_array, (const void*)(eeprom_ptr[1]), NUM_TIME_PARAMS);
+    eeprom_read_block((void*)SRAM_temp_array, (const void*)(eeprom_ptr[2]), NUM_TEMP_PARAMS);
+    
+    for (uint8_t i=0; i < NAMESIZE; i++)
+    {
+        if (SRAM_name_array[i] != '\0')
+        {   
+        (_prof->name)[i] = SRAM_name_array[i];
+        }
+    }
+    
+    for (uint8_t i=0; i < NUM_TIME_PARAMS; i++)
+    {
+        (_prof->Time_array)[i] = SRAM_time_array[i];
+    }        
+    
+    for (uint8_t i=0; i < NUM_TEMP_PARAMS; i++)
+    {
+        (_prof->Temp_array)[i] = SRAM_temp_array[i];
+    }
+    
+    return;
+}
+
+
+uint8_t get_Profile_time_param(Profile *_prof, uint8_t PARAMETER)
+{
+    return _prof->Time_array[PARAMETER];
+}
+
+uint8_t get_Profile_temp_param(Profile *_prof, uint8_t PARAMETER)
+{
+    return _prof->Temp_array[PARAMETER];
+}
--- reflow_oven/ProfileFunctions.h
+++ reflow_oven/ProfileFunctions.h
@@ -11,13 +11,23 @@
 
 #include "reflow_EEPROM.h"
 
-typedef struct profile
+typedef struct profile_struct
 {
-    char name[NAMESIZE];
+    uint8_t name[NAMESIZE];
     uint8_t Time_array[NUM_TIME_PARAMS];
     uint8_t Temp_array[NUM_TEMP_PARAMS];
     
 } Profile;
+
+
+void update_Profile(Profile *_prof, uint8_t **eeprom_ptr);
+/*  Pass in 'working_profile', and the array of eeprom ptrs (eeprom_ptr) to update working profile values.          */
+
+uint8_t get_Profile_time_param(Profile *_prof, uint8_t PARAMETER);
+
+uint8_t get_Profile_temp_param(Profile *_prof, uint8_t PARAMETER);
+
+
 
 
 #endif /* PROFILEFUNCTIONS_H_ */
